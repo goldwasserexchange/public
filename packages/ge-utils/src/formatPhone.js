93bc9d3ce6bf9b5c7144e654c949104bf802cb92
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2017-04-18 12:11:53 +0200

    Fix formatPhone for local numbers and unknown formats

--- packages/ge-utils/src/formatPhone.js
+++ packages/ge-utils/src/formatPhone.js
@@ -1,17 +1,7 @@
 import R from 'ramda';
-import startsWith from './startsWith';
-
-const startsWith00 = startsWith('00');
-const startsWithPlus = startsWith('+');
 
 export default R.pipe(
   R.replace(/[^\d+]/g, ''),
-  R.unless(
-    startsWithPlus,
-    R.ifElse(
-      startsWith00,
-      R.replace('00', '+'),
-      R.replace('0', '+32')
-    )
-  )
+  R.when(R.test(/^00[1-9][\d]{8,14}$/), R.replace(/^00/, '+')),
+  R.when(R.test(/^0[\d]{8,9}$/), R.replace(/^0/, '+32'))
 );
