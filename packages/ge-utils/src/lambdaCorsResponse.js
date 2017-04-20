ff790cb456dd889d108ba1fd83c7b8386c5dda81
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2017-04-20 11:06:07 +0200

    Fix missing request headers in lambdaCorsResponse

--- packages/ge-utils/src/lambdaCorsResponse.js
+++ packages/ge-utils/src/lambdaCorsResponse.js
@@ -3,7 +3,7 @@ import lambdaResponse from './lambdaResponse';
 
 const addCorsHeaders = R.merge({ 'Access-Control-Allow-Origin': '*' });
 
-const lambdaCorsResponse = (statusCode, body, headers) =>
-  lambdaResponse(statusCode, body, addCorsHeaders(headers));
+const lambdaCorsResponse = (statusCode, body, resHeaders, reqHeaders) =>
+  lambdaResponse(statusCode, body, addCorsHeaders(resHeaders), reqHeaders);
 
 export default lambdaCorsResponse;
