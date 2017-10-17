5e8b54689f7cc964730d25f568700edf9f9659ae
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2017-10-17 12:25:32 +0200

    Update index generation script

--- packages/ge-utils/scripts/generateIndex.js
+++ packages/ge-utils/scripts/generateIndex.js
@@ -5,22 +5,17 @@ import R from 'ramda';
 const listFiles = () => fs.readdirSync(path.join(process.cwd(), 'src'));
 const removeIndex = R.filter(R.complement(R.equals('index.js')));
 const moduleName = file => path.basename(file, '.js');
-const importLine = file => `import ${moduleName(file)} from './${moduleName(file)}';`;
+const importLine = file => `export { default as ${moduleName(file)} } from './${moduleName(file)}';`;
 const importLines = R.pipe(
   R.map(importLine),
   R.join('\n'),
   R.concat(R.__, '\n\n') // eslint-disable-line no-underscore-dangle
 );
-const exportLines = R.pipe(
-  R.map(R.compose(s => `  ${s}`, moduleName)),
-  R.join(',\n'),
-  s => `export {\n${s},\n};\n`
-);
 
 const generateIndex = R.pipe(
   listFiles,
   removeIndex,
-  R.converge(R.concat, [importLines, exportLines])
+  importLines
 );
 
 fs.writeFileSync(path.join(process.cwd(), 'src', 'index.js'), generateIndex());
--- packages/ge-utils/src/index.js
+++ packages/ge-utils/src/index.js
@@ -1,61 +1,33 @@
-import actorAccountCommunication from './actorAccountCommunication';
-import actorAccountMod from './actorAccountMod';
-import awsEvent from './awsEvent';
-import formatIban from './formatIban';
-import formatPhone from './formatPhone';
-import isActorAccount from './isActorAccount';
-import lambdaCorsResponse from './lambdaCorsResponse';
-import lambdaHandler from './lambdaHandler';
-import lambdaResponse from './lambdaResponse';
-import log from './log';
-import moveCharsToEnd from './moveCharsToEnd';
-import onlyDigits from './onlyDigits';
-import parseInt10 from './parseInt10';
-import removeSpaces from './removeSpaces';
-import renameKeys from './renameKeys';
-import s3Event from './s3Event';
-import s3EventBucket from './s3EventBucket';
-import s3EventKey from './s3EventKey';
-import safeHead from './safeHead';
-import safeParse from './safeParse';
-import snsEvent from './snsEvent';
-import snsEventMessage from './snsEventMessage';
-import snsEventRawMessage from './snsEventRawMessage';
-import snsEventSubject from './snsEventSubject';
-import startsWith from './startsWith';
-import stringModulo from './stringModulo';
-import validActorAccount from './validActorAccount';
-import validIban from './validIban';
-import zipMapping from './zipMapping';
+export { default as actorAccountCommunication } from './actorAccountCommunication';
+export { default as actorAccountMod } from './actorAccountMod';
+export { default as awsEvent } from './awsEvent';
+export { default as eqProps } from './eqProps';
+export { default as etag } from './etag';
+export { default as formatIban } from './formatIban';
+export { default as formatPhone } from './formatPhone';
+export { default as isActorAccount } from './isActorAccount';
+export { default as lambdaCorsResponse } from './lambdaCorsResponse';
+export { default as lambdaHandler } from './lambdaHandler';
+export { default as lambdaResponse } from './lambdaResponse';
+export { default as log } from './log';
+export { default as moveCharsToEnd } from './moveCharsToEnd';
+export { default as onlyDigits } from './onlyDigits';
+export { default as parseInt10 } from './parseInt10';
+export { default as removeSpaces } from './removeSpaces';
+export { default as renameKeys } from './renameKeys';
+export { default as round } from './round';
+export { default as s3Event } from './s3Event';
+export { default as s3EventBucket } from './s3EventBucket';
+export { default as s3EventKey } from './s3EventKey';
+export { default as safeHead } from './safeHead';
+export { default as safeParse } from './safeParse';
+export { default as snsEvent } from './snsEvent';
+export { default as snsEventMessage } from './snsEventMessage';
+export { default as snsEventRawMessage } from './snsEventRawMessage';
+export { default as snsEventSubject } from './snsEventSubject';
+export { default as startsWith } from './startsWith';
+export { default as stringModulo } from './stringModulo';
+export { default as validActorAccount } from './validActorAccount';
+export { default as validIban } from './validIban';
+export { default as zipMapping } from './zipMapping';
 
-export {
-  actorAccountCommunication,
-  actorAccountMod,
-  awsEvent,
-  formatIban,
-  formatPhone,
-  isActorAccount,
-  lambdaCorsResponse,
-  lambdaHandler,
-  lambdaResponse,
-  log,
-  moveCharsToEnd,
-  onlyDigits,
-  parseInt10,
-  removeSpaces,
-  renameKeys,
-  s3Event,
-  s3EventBucket,
-  s3EventKey,
-  safeHead,
-  safeParse,
-  snsEvent,
-  snsEventMessage,
-  snsEventRawMessage,
-  snsEventSubject,
-  startsWith,
-  stringModulo,
-  validActorAccount,
-  validIban,
-  zipMapping,
-};
