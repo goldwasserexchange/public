8fae137b060c7a705c63c14463c1f46f36d317e4
hans_scheuren <hans@goldwasserexchange.be>
2017-11-03 16:48:03 -0400

    refactor(eslint-config-style): create eslint-config-style
    affects: @goldwasserexchange/eslint-config-style
    
    create eslint-config-style to implement Goldwasser Exchange ES Lint code style config

---
+++ packages/eslint-config-style/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": "airbnb/rules/style",
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "rules": {
+    "comma-dangle": [
+      2,
+      "always-multiline"
+    ],
+    "indent": [
+      2,
+      2,
+      {
+        "SwitchCase": 1
+      }
+    ],
+    "max-len": 0,
+  },
+};
