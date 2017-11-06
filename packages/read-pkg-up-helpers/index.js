1f58861a89595a7d2ab8c4351ac9d8690cc8142e
hans_scheuren <hans@goldwasserexchange.be>
2017-11-06 16:02:11 -0500

    feat(read-pkg-up-helpers): create resolveBin util
    affects: @goldwasserexchange/read-pkg-up-helpers
    
    create a util to resolve the bin against the upper closest package.json

--- packages/read-pkg-up-helpers/index.js
+++ packages/read-pkg-up-helpers/index.js
@@ -3,6 +3,7 @@
 const readPkgUp = require('read-pkg-up');
 const fs = require('fs');
 const path = require('path');
+const which = require('which');
 const arrify = require('arrify');
 const { has } = require('lodash');
 
@@ -25,6 +26,30 @@ const hasAnyDep = args => [hasDep, hasDevDep, hasPeerDep].some(fn => fn(args))
 
 const ifAnyDep = (deps, t, f) => (hasAnyDep(arrify(deps)) ? t : f)
 
+const resolveBin = (modName, {executable = modName, cwd = process.cwd()} = {}) => {
+  let pathFromWhich
+  try {
+    pathFromWhich = fs.realpathSync(which.sync(executable))
+  } catch (_error) {
+    // ignore _error
+  }
+  try {
+    const modPkgPath = require.resolve(`${modName}/package.json`)
+    const modPkgDir = path.dirname(modPkgPath)
+    const {bin} = require(modPkgPath)
+    const binPath = typeof bin === 'string' ? bin : bin[executable]
+    const fullPathToBin = path.join(modPkgDir, binPath)
+    if (fullPathToBin === pathFromWhich) {
+      return executable
+    }
+    return fullPathToBin.replace(cwd, '.')
+  } catch (error) {
+    if (pathFromWhich) {
+      return executable
+    }
+    throw error
+  }
+}
 
 module.exports = {
   hasFile,
@@ -35,4 +60,5 @@ module.exports = {
   hasDevDep,
   hasAnyDep,
   ifAnyDep,
+  resolveBin,
 };
