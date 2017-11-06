274405e4595ca7df38c336b7f5caee217bb2e5f5
hans_scheuren <hans@goldwasserexchange.be>
2017-11-06 13:58:02 -0500

    feat(read-pkg-up-helpers): create read-pkg-up-helpers
    affects: @goldwasserexchange/read-pkg-up-helpers
    
    create an util library to get information from the closest package.json going up in the directory
    tree

---
+++ packages/read-pkg-up-helpers/index.js
@@ -0,0 +1,32 @@
+// inspired by https://github.com/kentcdodds/kcd-scripts/blob/2d2a1b0b9bea5f772ffefa3144e5bf2c9264e3d0/src/utils.js
+
+const readPkgUp = require('read-pkg-up');
+const fs = require('fs');
+const arrify = require('arrify');
+const lodash = require('lodash');
+
+const { pkg } = readPkgUp.sync({
+  cwd: fs.realpathSync(process.cwd()),
+});
+
+const hasPkgProp = props => arrify(props).some(prop => lodash.has(pkg, prop))
+
+const hasPkgSubProp = pkgProp => props => hasPkgProp(arrify(props).map(p => `${pkgProp}.${p}`));
+
+const hasPeerDep = hasPkgSubProp('peerDependencies');
+const hasDep = hasPkgSubProp('dependencies');
+const hasDevDep = hasPkgSubProp('devDependencies');
+const hasAnyDep = args => [hasDep, hasDevDep, hasPeerDep].some(fn => fn(args))
+
+const ifAnyDep = (deps, t, f) => (hasAnyDep(arrify(deps)) ? t : f)
+
+
+module.exports = {
+  hasPkgProp,
+  hasPkgSubProp,
+  hasPeerDep,
+  hasDep,
+  hasDevDep,
+  hasAnyDep,
+  ifAnyDep,
+};
