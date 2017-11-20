696ddb4a52db9e05a0fa401014e5d9be38e7b280
hans_scheuren <hans@goldwasserexchange.be>
2017-11-20 10:53:45 -0500

    feat(babel-preset/getPresetEnv): use ifAnyDep in target of getPresetEnv
    affects: @goldwasserexchange/babel-preset
    
    use ifAnyDep to determine the target of babel-preset-env depending on react installation

--- packages/babel-preset/getPresetEnv/getTargets.js
+++ packages/babel-preset/getPresetEnv/getTargets.js
@@ -1,17 +1,25 @@
+const { ifAnyDep } = require('@goldwasserexchange/read-pkg-up-helpers');
+
 const browsers = require('@goldwasserexchange/browserslist');
 
-const getTargets = env => (
-  (env === 'test')
-    ? {
-      node: 'current',
-    }
-    : {
-      // Browserlist support
-      browsers,
-      // We currently minify with uglify
-      // Remove after https://github.com/mishoo/UglifyJS2/issues/448
-      uglify: true,
-    }
+const node = {
+  node: 'current',
+};
+
+const browser = env => (env !== 'test')
+  ? {
+    // Browserlist support
+    browsers,
+    // We currently minify with uglify
+    // Remove after https://github.com/mishoo/UglifyJS2/issues/448
+    uglify: true,
+  }
+  : node;
+
+const getTargets = env => ifAnyDep(
+  'react',
+  browser(env),
+  node
 );
 
 module.exports = getTargets;
