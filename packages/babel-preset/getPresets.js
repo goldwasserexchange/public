3344de632e5560a4861cb8ef4e94f5bd5e91f134
hans_scheuren <hans@goldwasserexchange.be>
2017-11-20 12:29:50 -0500

    feat(babel-preset/getPresets): use ifAnyDep in getPresets
    affects: @goldwasserexchange/babel-preset
    
    use ifAnyDep in getPresets to only get the needed presets for the build

--- packages/babel-preset/getPresets.js
+++ packages/babel-preset/getPresets.js
@@ -1,10 +1,15 @@
+const { ifAnyDep } = require('@goldwasserexchange/read-pkg-up-helpers');
+
 const getPresetEnv = require('./getPresetEnv');
 
 const getPresets = env => [
   // ES features necessary for user's Node version
   getPresetEnv(env),
   // JSX, Flow
-  require.resolve('babel-preset-react'),
+  ifAnyDep(
+    'react',
+    require.resolve('babel-preset-react')
+  ),
 ];
 
 module.exports = getPresets;
