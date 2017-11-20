9a610698ba2d5b5792935c121e758d14d55dde35
hans_scheuren <hans@goldwasserexchange.be>
2017-11-20 11:05:29 -0500

    feat(babel-preset/getPresetEnv): use ifAnyDep to determine babel-preset-env module
    affects: @goldwasserexchange/babel-preset
    
    use ifAnyDep to determine the right configuration for babel-preset-env module option

--- packages/babel-preset/getPresetEnv/index.js
+++ packages/babel-preset/getPresetEnv/index.js
@@ -1,7 +1,10 @@
-
+const { ifAnyDep } = require('@goldwasserexchange/read-pkg-up-helpers');
 const getTargets = require('./getTargets');
 
 const requirePresetEnv = require('./requirePresetEnv');
+
+const node = 'commonjs';
+const browser = env => env === 'test' ? 'commonjs' : false;
 
 module.exports = env => [
   requirePresetEnv(env),
@@ -10,6 +13,10 @@ module.exports = env => [
     // Disable polyfill transforms
     useBuiltIns: false,
     // transform modules to CJS only on test
-    modules: env === 'test' ? 'commonjs' : false,
+    modules:ifAnyDep(
+      'react',
+      browser(env),
+      node
+    )
   },
 ];
