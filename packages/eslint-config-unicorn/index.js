e7df79460f1f86c45c3de0faae24e3a6bc3d71ef
hans_scheuren <hans@goldwasserexchange.be>
2017-12-07 17:14:04 -0500

    feat(eslint-config-unicorn): create eslint-config-unicorn
    affects: @goldwasserexchange/eslint-config-unicorn
    
    create unicorn eslint config for goldwasserexchange

---
+++ packages/eslint-config-unicorn/index.js
@@ -0,0 +1,38 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "plugins": [
+    require.resolve("eslint-plugin-unicorn"),
+  ],
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "rules": {
+    "unicorn/catch-error-name": ["error", {"name": "err"}],
+    "unicorn/explicit-length-check": "error",
+    "unicorn/filename-case": "off",
+    "unicorn/no-abusive-eslint-disable": "error",
+    "unicorn/no-process-exit": "error",
+    "unicorn/throw-new-error": "error",
+    "unicorn/number-literal-case": "error",
+    "unicorn/escape-case": "error",
+    "unicorn/no-array-instanceof": "error",
+    "unicorn/no-new-buffer": "error",
+    "unicorn/no-hex-escape": "error",
+    "unicorn/custom-error-definition": "error",
+    "unicorn/prefer-starts-ends-with": "error",
+    "unicorn/prefer-type-error": "error",
+    "unicorn/no-fn-reference-in-iterator": "error",
+    "unicorn/import-index": "error",
+    "unicorn/new-for-builtins": "error",
+    "unicorn/regex-shorthand": "error"
+  }
+};
