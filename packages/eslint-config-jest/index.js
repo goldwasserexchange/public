743644466df055ff7e656d834e584607dc008c9f
hans_scheuren <hans@goldwasserexchange.be>
2017-12-11 07:43:17 -0500

    fix(eslint-config-jest): use overrides to only apply jest rules to test files
    affects: @goldwasserexchange/eslint-config-jest
    
    use a overrides block in eslint-config-jest shareable eslint config to only apply jest rules to
    files ending with .test.js

--- packages/eslint-config-jest/index.js
+++ packages/eslint-config-jest/index.js
@@ -16,13 +16,30 @@ module.exports = {
     }
   },
   "rules": {
-    "jest/no-disabled-tests": "error",
-    "jest/no-focused-tests": "error",
-    "jest/no-identical-title": "error",
-    "jest/no-large-snapshots": "warn",
-    "jest/prefer-to-have-length": "error",
-    "jest/prefer-to-be-null": "error",
-    "jest/prefer-to-be-undefined": "error",
-    "jest/valid-expect": "error",
-  }
+    "jest/no-disabled-tests": "off",
+    "jest/no-focused-tests": "off",
+    "jest/no-identical-title": "off",
+    "jest/no-large-snapshots": "off",
+    "jest/prefer-to-have-length": "off",
+    "jest/prefer-to-be-null": "off",
+    "jest/prefer-to-be-undefined": "off",
+    "jest/valid-expect": "off",
+  },
+  "overrides": [
+    {
+      "files": [
+        '**/*.test.js'
+      ],
+      "rules": {
+        "jest/no-disabled-tests": "error",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/no-large-snapshots": "warn",
+        "jest/prefer-to-have-length": "error",
+        "jest/prefer-to-be-null": "error",
+        "jest/prefer-to-be-undefined": "error",
+        "jest/valid-expect": "error",
+      }
+    }
+  ]
 };
