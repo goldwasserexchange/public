6ef56c412b10dbd01eb99d4e614a96d30c82b108
hans_scheuren <hans@goldwasserexchange.be>
2017-12-13 13:25:51 -0500

    feat(colors): create colors to keep the colors used by Goldwasser Exchange
    affects: @goldwasserexchange/colors

---
+++ packages/colors/index.js
@@ -0,0 +1,18 @@
+export default {
+  white: '#FFFFFF',
+  red: '#d9534f',
+  green: '#2ECC40',
+  blue: '#3394d1',
+  whiteblue: '#e3f2fb',
+  lightblue: '#85C8EF',
+  middleblue: '#3394D1',
+  elementmiddleblue: '#76beea',
+  darkblue: '#145098',
+  elementdarkblue: '#146ca3',
+  elementdarkerblue: '#c4dae8',
+  logoblue: '#21346c',
+  linkBlue: '#3294d1',
+  gwyellow: '#F7CA38',
+  yellow: '#FFDC00',
+  black: '#666666',
+};
