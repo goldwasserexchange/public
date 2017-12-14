653938a0e92898231daed4bcd982848bebae5873
hans_scheuren <hans@goldwasserexchange.be>
2017-12-14 14:22:21 -0500

    feat(style-vars): create goldwasserexchange/style-vars
    affects: @goldwasserexchange/style-vars
    
    create goldwasserexchange/style-vars to hold Goldwasser Exchange style variables

---
+++ packages/style-vars/src/index.js
@@ -0,0 +1,9 @@
+import colors from '@goldwasserexchange/colors';
+import fonts from '@goldwasserexchange/fonts';
+import fontWeights from '@goldwasserexchange/font-weights';
+
+export default {
+  ...colors,
+  ...fonts,
+  ...fontWeights,
+}
