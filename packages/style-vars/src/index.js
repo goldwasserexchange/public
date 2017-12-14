6fc598aea5f5ecebc7b43c2f46edc87442e685a3
hans_scheuren <hans@goldwasserexchange.be>
2017-12-14 16:24:25 -0500

    feat(style-vars): use goldwasserexchange/widths in style-vars
    affects: @goldwasserexchange/style-vars

--- packages/style-vars/src/index.js
+++ packages/style-vars/src/index.js
@@ -1,9 +1,11 @@
 import colors from '@goldwasserexchange/colors';
 import fonts from '@goldwasserexchange/fonts';
 import fontWeights from '@goldwasserexchange/font-weights';
+import widths from '@goldwasserexchange/widths';
 
 export default {
   ...colors,
   ...fonts,
   ...fontWeights,
+  ...widths,
 }
