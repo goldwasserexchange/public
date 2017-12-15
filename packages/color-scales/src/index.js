785d04d6b93454acd01256044f7e5356cc0a3f29
hans_scheuren <hans@goldwasserexchange.be>
2017-12-15 10:50:04 -0500

    fix(color-scales): export color scales individually
    affects: @goldwasserexchange/color-scales

--- packages/color-scales/src/index.js
+++ packages/color-scales/src/index.js
@@ -2,6 +2,10 @@ import blueShort from './scales/blueShort';
 import blueExtended from './scales/blueExtended';
 import negativePositive from './scales/negativePositive';
 
+export blueShort;
+export blueExtended;
+export negativePositive;
+
 export default {
   blueShort,
   blueExtended,
