777e14441dc385a56957ba7f6922f6915d3aa141
hans_scheuren <hans@goldwasserexchange.be>
2017-12-15 15:03:49 -0500

    fix(widths): fix import in width
    affects: @goldwasserexchange/widths

--- packages/widths/index.js
+++ packages/widths/index.js
@@ -1,9 +1,9 @@
-const { breakPointsArbitrary: { centerContainerUpMin } = {} } = require('@goldwasserexchange/media-queries');
+const { breakPointsArbitrary: { centerContainer } = {} } = require('@goldwasserexchange/media-queries');
 const { operationOnUnitString } = require('@goldwasserexchange/utils');
 
 const widths = {
-  centerContainerWidth: centerContainerUpMin,
-  halfCenterContainerWidth: operationOnUnitString((x) => x / 2, centerContainerUpMin),
+  centerContainerWidth: centerContainer,
+  halfCenterContainerWidth: operationOnUnitString((x) => x / 2, centerContainer),
   selectButtonWidthSmallScreen: '3em',
   selectButtonWidthLargeScreen: '2.5em',
 }
