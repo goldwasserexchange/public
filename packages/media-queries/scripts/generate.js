0ab8084a7423c772dd0df6a20291f34460ea8a33
hans_scheuren <hans@goldwasserexchange.be>
2017-12-15 11:20:59 -0500

    feat(media-queries): create the generator and generate watchableMediaQueries
    affects: @goldwasserexchange/media-queries

--- packages/media-queries/scripts/generate.js
+++ packages/media-queries/scripts/generate.js
@@ -47,3 +47,16 @@ fs.writeFileSync(path.resolve(__dirname, '../index.js'),
 
 module.exports = mediaQueries;
 `)
+
+fs.writeFileSync(path.resolve(__dirname, '../watchableMediaQueries.js'),
+`const watchableMediaQueries = {
+  sm: '(min-width: ${minMaxBreakPoint.smMin})',
+  md: '(min-width: ${minMaxBreakPoint.mdMin})',
+  lg: '(min-width: ${minMaxBreakPoint.lgMin})',
+  print: 'print',
+  centerContainer: '(min-width: ${minMaxBreakPoint.centerContainerUpMin})',
+}
+
+module.exports = watchableMediaQueries;
+`
+)
---
+++ packages/media-queries/watchableMediaQueries.js
@@ -0,0 +1,9 @@
+const watchableMediaQueries = {
+  sm: '(min-width: 34em)',
+  md: '(min-width: 48em)',
+  lg: '(min-width: 62em)',
+  print: 'print',
+  centerContainer: '(min-width: 1300px)',
+}
+
+module.exports = watchableMediaQueries;
