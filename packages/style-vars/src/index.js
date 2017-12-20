51378915dbde19287cbb72d279caeef0cb2d9511
hans_scheuren <hans@goldwasserexchange.be>
2017-12-20 15:58:00 -0500

    refactor(style-vars): remove deprecated widths package
    affects: @goldwasserexchange/style-vars

--- packages/style-vars/src/index.js
+++ packages/style-vars/src/index.js
@@ -1,11 +1,11 @@
 import colors from '@goldwasserexchange/colors';
 import fonts from '@goldwasserexchange/fonts';
 import fontWeights from '@goldwasserexchange/font-weights';
-import widths from '@goldwasserexchange/widths';
+import sizes from '@goldwasserexchange/sizes';
 
 export default {
   ...colors,
   ...fonts,
   ...fontWeights,
-  ...widths,
+  ...sizes,
 }
