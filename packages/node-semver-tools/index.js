f5a43bb4a99cbcc93aa259df06f92a415a35529a
hans_scheuren <hans@goldwasserexchange.be>
2018-01-03 15:26:04 -0500

    feat(node-semver-tools): create node semver tools to get node versions from ranges
    affects: @goldwasserexchange/node-semver-tools

---
+++ packages/node-semver-tools/index.js
@@ -0,0 +1,8 @@
+const versions = require('./versions');
+const semver = require('semver');
+
+const minSatisfying = (range) => semver.minSatisfying(versions, range);
+
+module.exports = {
+  minSatisfying,
+};
