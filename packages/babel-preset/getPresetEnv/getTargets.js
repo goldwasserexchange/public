ccc0828c30e1c0e45dc850a565bb1ec624ec4834
hans_scheuren <hans@goldwasserexchange.be>
2018-02-19 18:06:50 +0100

    fix(babel-preset): remove uglify from getTargets
    affects: @goldwasserexchange/babel-preset

--- packages/babel-preset/getPresetEnv/getTargets.js
+++ packages/babel-preset/getPresetEnv/getTargets.js
@@ -2,7 +2,6 @@ const browsers = require('@goldwasserexchange/browserslist');
 
 const node = (nodeVersion) => ({
   node: nodeVersion.replace(/v/g, '') || 'current',
-  uglify: false,
 });
 
 const getTargets = (env, target, nodeVersion) => (target === 'node' || (target === 'browser' && env === 'test'))
