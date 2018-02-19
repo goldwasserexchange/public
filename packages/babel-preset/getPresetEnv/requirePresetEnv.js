06f83f3a05d10fabf4416e9a5cbca0dc590f1f05
hans_scheuren <hans@goldwasserexchange.be>
2018-02-19 11:58:14 +0100

    fix(babel-preset): fix #6
    affects: @goldwasserexchange/babel-preset
    
    replace babel-preset-env with @babel/preset-env

--- packages/babel-preset/getPresetEnv/requirePresetEnv.js
+++ packages/babel-preset/getPresetEnv/requirePresetEnv.js
@@ -1,7 +1,7 @@
 const { ifAnyDep } = require('@goldwasserexchange/read-pkg-up-helpers');
 
 const requirePresetEnv = (env, target) => (target === 'node' || (target === 'browser' && env === 'test'))
-  ? require.resolve('babel-preset-env')
-  : require('babel-preset-env').default;
+  ? require.resolve('@babel/preset-env')
+  : require('@babel/preset-env').default;
 
 module.exports = requirePresetEnv;
