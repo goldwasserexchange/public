b1dcc7971ea7e35599e6275743bfc0a9c1783672
hans_scheuren <hans@goldwasserexchange.be>
2018-02-19 11:59:51 +0100

    fix(babel-preset): fix getPresetEnv getTargets
    affects: @goldwasserexchange/babel-preset
    
    getTargets didn't handle node version and target

--- packages/babel-preset/getPresetEnv/getTargets.js
+++ packages/babel-preset/getPresetEnv/getTargets.js
@@ -1,26 +1,19 @@
-const { ifAnyDep } = require('@goldwasserexchange/read-pkg-up-helpers');
-
 const browsers = require('@goldwasserexchange/browserslist');
 
 const node = (nodeVersion) => ({
-  node: nodeVersion || 'current',
+  node: nodeVersion.replace(/v/g, '') || 'current',
   uglify: true,
 });
 
-const browser = (env, target, nodeVersion) => (target === 'node' || (target === 'browser' && env === 'test'))
-  ? node(nodeVersion)
-  : {
-    // Browserlist support
-    browsers,
-    // We currently minify with uglify
-    // Remove after https://github.com/mishoo/UglifyJS2/issues/448
-    uglify: true,
-  };
-
-const getTargets = env => ifAnyDep(
-  'react',
-  browser(env),
-  node
-);
+const getTargets = (env, target, nodeVersion) => {
+	const result = (target === 'node' || (target === 'browser' && env === 'test'))
+	  ? node(nodeVersion)
+	  : {
+      // Browserlist support
+      browsers,
+	  };
+	console.log(result);
+	return result;
+}
 
 module.exports = getTargets;
