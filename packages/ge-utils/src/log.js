c0eb81a38cb2609c5015ca0c08ee0ceb4810e12f
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2018-02-28 15:11:02 +0100

    Do not stringify errors in log

--- packages/ge-utils/src/log.js
+++ packages/ge-utils/src/log.js
@@ -2,4 +2,4 @@ import { curry } from 'ramda';
 import stringify from 'json-stringify-safe';
 
 export default curry((title, data) => // eslint-disable-next-line no-console
-  console.log(`${title}: ${stringify(data)}`));
+  console.log(`${title}: ${data instanceof Error ? data : stringify(data)}`));
