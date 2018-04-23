67264b62c01fb9e9127253865dd5dd55b19f5a02
hans_scheuren <hans@goldwasserexchange.be>
2018-04-23 23:40:11 +0200

    transpile ge-scripts with babel

---
+++ packages/ge-scripts/src/index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const spawn = require('cross-spawn');
+const messages = require('./messages');
+
+const [executor, /* eslint-disable no-unused-vars */ ignoredBin /* eslint-enable */, script, ...args] = process.argv;
+
+function handleSignal(result) {
+  console.log(messages[result.signal](script)); // eslint-disable-line no-console
+  process.exit(1);
+}
+
+function getEnv() {
+  // this is required to address an issue in cross-spawn
+  // https://github.com/kentcdodds/kcd-scripts/issues/4
+  return Object.keys(process.env)
+    .filter(key => process.env[key] !== undefined)
+    .reduce(
+      (envCopy, key) => ({
+        ...envCopy,
+        [key]: process.env[key],
+
+      }),
+      {
+        [`SCRIPTS_${script.toUpperCase()}`]: true,
+      },
+    );
+}
+
+if (script) {
+  let scriptPath;
+  try {
+    scriptPath = require.resolve(`@goldwasserexchange/${script}`);
+  } catch (err) {
+    console.log('error', err); // eslint-disable-line no-console
+  }
+  if (scriptPath) {
+    const result = spawn.sync(executor, [scriptPath, ...args], {
+      stdio: 'inherit',
+      env: getEnv(),
+    });
+
+    if (result.signal) {
+      handleSignal(result);
+    } else {
+      process.exit(result.status);
+    }
+  } else {
+    throw new Error(`Unknown script "${script}".`);
+  }
+}
