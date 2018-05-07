ba892278865bc433e367636a45de90e87717b8a7
hans_scheuren <hans@goldwasserexchange.be>
2018-05-07 22:05:00 +0200

    fix(ge-scripts): fix ge-scripts by using ramda instead of building
    affects: ge-scripts

--- packages/ge-scripts/index.js
+++ packages/ge-scripts/index.js
@@ -1,49 +1,43 @@
 #!/usr/bin/env node
-"use strict";
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 const spawn = require('cross-spawn');
-
 const messages = require('./messages');
+const { set, lensProp, view } = require('ramda');
 
-const [executor,
-/* eslint-disable no-unused-vars */
-ignoredBin
-/* eslint-enable */
-, script, ...args] = process.argv;
+const [executor, /* eslint-disable no-unused-vars */ ignoredBin /* eslint-enable */, script, ...args] = process.argv;
 
 function handleSignal(result) {
   console.log(messages[result.signal](script)); // eslint-disable-line no-console
-
   process.exit(1);
 }
 
 function getEnv() {
   // this is required to address an issue in cross-spawn
   // https://github.com/kentcdodds/kcd-scripts/issues/4
-  return Object.keys(process.env).filter(key => process.env[key] !== undefined).reduce((envCopy, key) => _objectSpread({}, envCopy, {
-    [key]: process.env[key]
-  }), {
-    [`SCRIPTS_${script.toUpperCase()}`]: true
-  });
+  return Object.keys(process.env)
+    .filter(key => process.env[key] !== undefined)
+    .reduce(
+      (envCopy, key) => {
+        const keyLens = lensProp(key);
+        return set(keyLens, view(keyLens, process.env), envCopy);
+      },
+      {
+        [`SCRIPTS_${script.toUpperCase()}`]: true,
+      },
+    );
 }
 
 if (script) {
   let scriptPath;
-
   try {
     scriptPath = require.resolve(`@goldwasserexchange/${script}`);
   } catch (err) {
     console.log('error', err); // eslint-disable-line no-console
   }
-
   if (scriptPath) {
     const result = spawn.sync(executor, [scriptPath, ...args], {
       stdio: 'inherit',
-      env: getEnv()
+      env: getEnv(),
     });
 
     if (result.signal) {
--- packages/ge-scripts/src/messages.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  SIGKILL: script => `The script "${script}" failed because the process exited too early. This probably means the system ran out of memory or someone called 'kill -9' on the process.`,
-  SIGTERM: script => `The script "${script}" failed because the process exited too early. Someone might have called 'kill' or 'killall', or the system could be shutting down.`,
-};
