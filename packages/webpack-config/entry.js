ee99eacc2484c97067ad50b2e1199a97bd6b9e80
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2018-08-20 21:17:53 +0200

    fix(webpack-config): remove whatwg-fetch entry when target is not web
    affects: @goldwasserexchange/webpack-config

--- packages/webpack-config/entry.js
+++ packages/webpack-config/entry.js
@@ -1,12 +1,11 @@
 const path = require('path');
 const { getPkgSrc, hasAnyDep } = require('@goldwasserexchange/read-pkg-up-helpers');
-const webpackPkgConfig = require('./webpackPkgConfig');
+const { target } = require('./webpackPkgConfig');
 
 module.exports = [
-  (process.env.NODE_ENV === 'production' && webpackPkgConfig.target === 'web') && '@babel/polyfill',
-  (process.env.NODE_ENV === 'production' && webpackPkgConfig.target === 'web') && 'whatwg-fetch',
+  (process.env.NODE_ENV === 'production' && target === 'web') && '@babel/polyfill',
+  (process.env.NODE_ENV === 'production' && target === 'web') && 'whatwg-fetch',
   (process.env.NODE_ENV === 'development' && hasAnyDep('webpack-hot-middleware')) && 'eventsource-polyfill', // Necessary for hot reloading with IE
   (process.env.NODE_ENV === 'development' && hasAnyDep('webpack-hot-middleware')) && 'webpack-hot-middleware/client?reload=true',
-
   path.join(process.cwd(), getPkgSrc()), // Start with js/app.js
 ].filter(Boolean);
