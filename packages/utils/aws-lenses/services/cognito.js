20932863f34f8c02a451664922f7e598baef8a82
Hans Scheuren <hans@goldwasserexchange.be>
2018-08-23 14:32:44 +0200

    refactor(all): refactor directory structure
    affects: @goldwasserexchange/babel-preset, @goldwasserexchange/browserslist,
    @goldwasserexchange/postcss-config, @goldwasserexchange/replace-in-chunk-webpack-plugin,
    @goldwasserexchange/webpack-config, @goldwasserexchange/webpack-server, @goldwasserexchange/babel,
    ge-scripts, @goldwasserexchange/inject-eslint-config, @goldwasserexchange/lint-js,
    @goldwasserexchange/eslint-config-base, @goldwasserexchange/eslint-config-import,
    @goldwasserexchange/eslint-config-jest, @goldwasserexchange/eslint-config-ramda,
    @goldwasserexchange/eslint-config-react, @goldwasserexchange/eslint-config-strict,
    @goldwasserexchange/eslint-config-style, @goldwasserexchange/eslint-config-unicorn,
    @goldwasserexchange/eslint-config, @goldwasserexchange/color-scales, @goldwasserexchange/colors,
    @goldwasserexchange/font-weights, @goldwasserexchange/fonts, @goldwasserexchange/media-queries,
    @goldwasserexchange/sizes, @goldwasserexchange/style-vars, @goldwasserexchange/aws-lenses,
    @goldwasserexchange/node-semver-tools, @goldwasserexchange/read-pkg-up-helpers,
    @goldwasserexchange/utils, @goldwasserexchange/write-npmrc

---
+++ packages/utils/aws-lenses/services/cognito.js
@@ -0,0 +1,15 @@
+import compose from "ramda/es/compose";
+import lensProp from "ramda/es/lensProp";
+export const username = lensProp('userName');
+export const triggerSource = lensProp('triggerSource');
+export const request = lensProp('request');
+export const userAttributes = compose(request, lensProp('userAttributes'));
+export const birthdate = compose(userAttributes, lensProp('birthdate'));
+export const phoneNumber = compose(userAttributes, lensProp('phone_number'));
+export const locale = compose(userAttributes, lensProp('locale'));
+export const codeParameter = compose(request, lensProp('codeParameter'));
+export const validationData = compose(request, lensProp('validationData'));
+export const response = lensProp('response');
+export const smsMessage = compose(response, lensProp('smsMessage'));
+export const claimsOverrideDetails = compose(response, lensProp('claimsOverrideDetails'));
+export const claimsToAddOrOverride = compose(claimsOverrideDetails, lensProp('claimsToAddOrOverride'));
