5971128170bddeaeb130152672f18305bf2112b1
Hans Scheuren <hans@goldwasserexchange.be>
2018-09-27 11:22:44 +0200

    fix(webpack-config): getStartUrl shouldn't be specific to our frontend

--- packages/build-tools/webpack/webpack-config/utils/getStartUrl.js
+++ packages/build-tools/webpack/webpack-config/utils/getStartUrl.js
@@ -1,10 +1,5 @@
+const { startUrls = {} } = require('../webpackPkgConfig');
+
 module.exports = function getStartUrl() {
-  let startUrl = 'http://localhost:3000';
-  if (process.env.BITBUCKET_BRANCH && process.env.BITBUCKET_BRANCH === 'master') {
-    startUrl = 'https://dev.goldwasseronline.be';
-  }
-  if (process.env.BITBUCKET_TAG && process.env.BITBUCKET_TAG[0] === 'v') {
-    startUrl = 'https://www.goldwasseronline.be';
-  }
-  return startUrl;
+  return startUrls[process.env.STAGE || 'default'] || 'http://localhost:3000';
 };
