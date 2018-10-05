aa47ed7bcf4265f35b68358a26afdccc3b476d29
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2018-10-05 09:49:21 +0200

    fix(webpack-config): enable jarvis, dashboard and workbox plugins only when target is web
    Closes #63

--- packages/build-tools/webpack/webpack-config/plugins/config/jarvis.js
+++ packages/build-tools/webpack/webpack-config/plugins/config/jarvis.js
@@ -1,7 +1,8 @@
 const Jarvis = require('webpack-jarvis');
 const { isNil } = require('ramda');
+const { target } = require('../../webpackPkgConfig');
 
-module.exports = new Jarvis({
+module.exports = target === 'web' && new Jarvis({
   port: 1337,
   watchOnly: isNil(process.env.JARVIS_WATCH) && process.env.JARVIS_WATCH !== '0' ? true : process.env.JARVIS_WATCH !== '0',
 });
