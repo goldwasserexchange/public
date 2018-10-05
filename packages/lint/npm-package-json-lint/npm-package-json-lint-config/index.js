3047c3868b730038ca74a97707f18646da947816
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2018-10-05 15:05:45 +0200

    feat(npm-package-json-lint-config): add package npm-package-json-lint-config

---
+++ packages/lint/npm-package-json-lint/npm-package-json-lint-config/index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: require.resolve('npm-package-json-lint-config-default'),
+  rules: {
+    'require-homepage': 'error',
+    'require-publishConfig': 'error',
+    'require-repository': 'error',
+    'require-license': 'error',
+    'valid-values-license': ['error', [
+      'MIT',
+      'UNLICENSED',
+    ]],
+    'valid-values-publishConfig': ['error', [
+      { access: 'public' },
+    ]],
+    'prefer-caret-version-dependencies': 'warning',
+    'prefer-alphabetical-dependencies': 'error',
+    'prefer-caret-version-devDependencies': 'warning',
+    'prefer-alphabetical-devDependencies': 'error',
+    'prefer-alphabetical-peerDependencies': 'error',
+    'description-format': ['error', {
+      requireCapitalFirstLetter: true,
+    }],
+  },
+};
