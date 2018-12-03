83ea573e69b81bda1118f00bcda5ab9199546715
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2018-12-03 21:46:15 +0100

    feat(webpack-config): move startUrls to manifest key in package webpack config

--- packages/build-tools/webpack/webpack-config/utils/getStartUrl.js
+++ packages/build-tools/webpack/webpack-config/utils/getStartUrl.js
@@ -1,4 +1,4 @@
-const { startUrls = {} } = require('../webpackPkgConfig');
+const { manifest: { startUrls = {} } = {} } = require('../webpackPkgConfig');
 
 module.exports = function getStartUrl() {
   return startUrls[process.env.STAGE || 'default'] || 'http://localhost:3000';
