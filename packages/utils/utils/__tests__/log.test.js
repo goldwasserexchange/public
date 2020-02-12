d997169f198d9cfd22917d50ff75e792d53c7938
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2020-02-12 09:09:03 +0100

    chore(deps): massive dependencies upgrade

--- packages/utils/utils/__tests__/log.test.js
+++ packages/utils/utils/__tests__/log.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import serializeError from 'serialize-error';
+import { serializeError } from 'serialize-error';
 import log from '../src/log';
 
 console.log = jest.fn();
